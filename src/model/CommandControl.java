dfa4f312058942bcfaf4b52182a0fcb2f0f2b03e
Gemi <romaint69@gmail.com>
2016-01-26 14:04:50 +0100

    parser method

--- src/model/CommandControl.java
+++ src/model/CommandControl.java
@@ -4,6 +4,7 @@ package model;/*
  * and open the template in the editor.
  */
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Stack;
 
@@ -12,23 +13,47 @@ import java.util.Stack;
  */
 public abstract class CommandControl {
 
-    public static void parser(String command){
+    public static void parser(Data data, String command){
         if(validParenthesage(command)) {
+            //split by ' ' between CRUD and node or relation
+            //like "CREATE (Paul:Person),(Sarah:Person{age:'25',ville:'Lyon'})"
             String[] parts = command.split(" ");
-            if(parts[0] == "MATCH"){
+            if(parts[0].equals("MATCH")){
 
             }
-            else if(parts[0] == "CREATE"){
+            else if(parts[0].equals("CREATE")){
                 if(validParenthesage(parts[1])){
-                    String[] subparts = parts[1].split("()");
-                    for(String s : subparts)
-                        System.out.println(s);
+                    //split by ',' between nodes
+                    String[] subparts = parts[1].split(",");
+                    //for each parts between the ','
+                    for(String s : subparts) {
+                        //remove parentheses
+                        String[] subpart = s.split("\\("); subpart = subpart[1].split("\\)");
+                        //split by '{'
+                        String[] propSplit = subpart[0].split("\\{");
+                        ArrayList<String> propList = new ArrayList();
+                        if(propSplit.length > 1) {
+                            //remove '}'
+                            String[] properties = propSplit[1].split("\\}");
+                            //split by ';'
+                            String[] propParts = properties[0].split(";");
+                            for(String p : propParts) propList.add(p);
+                        }
+                        //split by ':'
+                        String[] labels = propSplit[0].split(":");
+                        ArrayList<String> labelList = new ArrayList();
+                        for(int i = 1; i < labels.length; i++)
+                            labelList.add(labels[i]);
+                        data.getNodeList().add(new Node(labels[0], labelList, propList));
+                    }
                 }
+                else System.out.println("Commande invalide : erreur de parenthesage");
             }
-            else if(parts[0] == "DELETE"){
+            else if(parts[0].equals("DELETE")){
 
             }
         }
+        else System.out.println("Commande invalide : erreur de parenthesage");
     }
 
     public static boolean validParenthesage(String s) {
