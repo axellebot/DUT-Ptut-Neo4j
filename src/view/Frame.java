151a6a9343f5a3940c2b915536a32dd9c9981152
Milihhard <milihhard1996@gmail.com>
2016-01-27 16:26:32 +0100

    ajout CreatePanel

--- src/view/Frame.java
+++ src/view/Frame.java
@@ -14,12 +14,16 @@ import java.awt.event.ComponentListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.io.File;
+import model.Node;
+import model.Relation;
 
 public class Frame extends JFrame {
+
     Data data, dataCurrent;
     GraphPanel graphPanel;
     PromptPanel promptPanel;
     ToolsPanel toolsPanel;
+    CreatePanel createpanel;
 
     public Frame() {
         super("Neo4j");
@@ -33,9 +37,11 @@ public class Frame extends JFrame {
         graphPanel = new GraphPanel();
         promptPanel = new PromptPanel();
         toolsPanel = new ToolsPanel();
+        createpanel = new CreatePanel();
 
         //ajout d'observateurs
         dataCurrent.addObservateur(graphPanel);
+        dataCurrent.addObservateur(createpanel);
         System.out.println("nombre observateur: " + data.getNumberObervateur());
 
         //placement des panneaux
@@ -44,15 +50,18 @@ public class Frame extends JFrame {
         cont.fill = GridBagConstraints.BOTH;
         cont.gridx = 0;
         cont.gridy = 0;
-        cont.gridheight = 2;
+        cont.gridheight = 1;
         this.getContentPane().add(toolsPanel, cont);
+        cont.gridx = 0;
+        cont.gridy = 1;
+        cont.gridheight = 1;
+        this.getContentPane().add(createpanel, cont);
         cont.gridx = 1;
         cont.gridy = 0;
-        cont.gridheight = 1;
-
+        cont.gridheight = 2;
         this.getContentPane().add(graphPanel, cont);
         cont.gridx = 1;
-        cont.gridy = 1;
+        cont.gridy = 2;
         this.getContentPane().add(promptPanel, cont);
 
         this.pack();
@@ -61,17 +70,17 @@ public class Frame extends JFrame {
     public Data getData() {
         return data;
     }
-/*
-    public void setData(Data data) {
-        this.data.changeData(data);
-    }
-    */
+    /*
+     public void setData(Data data) {
+     this.data.changeData(data);
+     }
+     */
 
-    public class GraphPanel extends JPanel implements control.Observateur{
+    public class GraphPanel extends JPanel implements control.Observateur {
 
         public VisuGraph graph = new VisuGraph(data);
 
-        public GraphPanel(){
+        public GraphPanel() {
             this.setBorder(BorderFactory.createTitledBorder("Graph"));
             this.setLayout(new GridBagLayout());
             this.setPreferredSize(new Dimension(1000, 600));
@@ -82,16 +91,16 @@ public class Frame extends JFrame {
         /**
          * update data with Current Data
          */
-        public void update(){
+        public void update() {
             graph.update(dataCurrent);
             System.out.println("update de data");
         }
     }
 
     public class PromptPanel extends JPanel {
+
         public JTextArea txtPrompt = new JTextArea(400, 200);
         public JButton btnGenerate = new JButton("Generate");
-
 
         public PromptPanel() {
             this.setBorder(BorderFactory.createTitledBorder("Prompt"));
@@ -112,7 +121,6 @@ public class Frame extends JFrame {
             this.add(btnGenerate, cont);
             this.setPreferredSize(new Dimension(1000, 100));
 
-
             btnGenerate.addActionListener(e -> {
                 //Call parser
                 CommandControl.parser(data, txtPrompt.getText());
@@ -123,6 +131,7 @@ public class Frame extends JFrame {
     }
 
     public class ToolsPanel extends JPanel {
+
         //Create a file chooser
         final JFileChooser fileChooser = new JFileChooser();
         public JButton btnExport = new JButton("Exporter");
@@ -157,23 +166,23 @@ public class Frame extends JFrame {
             this.add(tVoisin);
             this.add(btnTest);
             this.add(btnVue);
-/*
-            this.setLayout(new GridBagLayout());
-            GridBagConstraints cont = new GridBagConstraints();
-            cont.fill = GridBagConstraints.BOTH;
-            cont.gridx = 0;
-            cont.gridy = 0;
-            this.add(btnExport, cont);
+            /*
+             this.setLayout(new GridBagLayout());
+             GridBagConstraints cont = new GridBagConstraints();
+             cont.fill = GridBagConstraints.BOTH;
+             cont.gridx = 0;
+             cont.gridy = 0;
+             this.add(btnExport, cont);
 
-            cont.gridx = 0;
-            cont.gridy = 1;
-            this.add(btnImport, cont);
+             cont.gridx = 0;
+             cont.gridy = 1;
+             this.add(btnImport, cont);
 
-            cont.gridx = 0;
-            cont.gridy = 2;
-            this.add(btnRead, cont);
-            */
-            this.setPreferredSize(new Dimension(150, 100));
+             cont.gridx = 0;
+             cont.gridy = 2;
+             this.add(btnRead, cont);
+             */
+            this.setPreferredSize(new Dimension(200, 200));
 
             btnExport.addActionListener(e -> {
                 //In response to a button click:
@@ -218,15 +227,73 @@ public class Frame extends JFrame {
                     System.out.println("Read command cancelled by user.");
                 }
             });
-            btnTest.addActionListener(e->{
+            btnTest.addActionListener(e -> {
                 dataCurrent.changeDataCurrent(data.getVoisin(tVoisin.getText()));
             });
-            btnVue.addActionListener(e->{
+            btnVue.addActionListener(e -> {
                 dataCurrent.changeDataCurrent(data);
             });
         }
 
     }
 
-}
+    public class CreatePanel extends JPanel implements control.Observateur {
 
+        public JTextField tCreateNode = new JTextField(10);
+        public JButton btCreateNode = new JButton("creer une node");
+
+        public JTextField tCreateRelation = new JTextField(10);
+        public DefaultComboBoxModel<Node> modelNode1, modelNode2;
+        public JComboBox<Node> cbNode1;
+        public JComboBox<Node> cbNode2;
+        public JButton btCreateRelation = new JButton("creer une relation");
+
+        public CreatePanel() {
+            this.setBorder(BorderFactory.createTitledBorder("Creation"));
+
+            modelNode1 = new DefaultComboBoxModel();
+            modelNode2 = new DefaultComboBoxModel();
+
+            cbNode1 = new JComboBox(modelNode1);
+            cbNode2 = new JComboBox(modelNode2);
+
+            cbNode1.setPreferredSize(new Dimension(150, 20));
+            cbNode2.setPreferredSize(new Dimension(150, 20));
+
+            update();
+            this.add(tCreateNode);
+            this.add(btCreateNode);
+            this.add(tCreateRelation);
+            this.add(cbNode1);
+            this.add(cbNode2);
+            this.add(btCreateRelation);
+            this.setPreferredSize(new Dimension(200, 200));
+            btCreateNode.addActionListener(e -> {
+                data.addNode(new Node(tCreateNode.getText()));
+                dataCurrent.changeDataCurrent(data);
+                tCreateNode.setText("");
+            });
+            btCreateRelation.addActionListener(e -> {
+                if (!modelNode1.getSelectedItem().equals(modelNode2.getSelectedItem())) {
+                    data.addRelation(new Relation(tCreateRelation.getText(), (Node) modelNode1.getSelectedItem(), (Node) modelNode2.getSelectedItem()));
+                    dataCurrent.changeDataCurrent(data);
+                }
+                tCreateRelation.setText("");
+            });
+        }
+
+        @Override
+        public void update() {
+            modelNode1.removeAllElements();
+            modelNode2.removeAllElements();
+            for (Node n : dataCurrent.getNodeList()) {
+                modelNode1.addElement(n);
+            }
+            for (Node n : dataCurrent.getNodeList()) {
+                modelNode2.addElement(n);
+            }
+        }
+
+    }
+
+}
